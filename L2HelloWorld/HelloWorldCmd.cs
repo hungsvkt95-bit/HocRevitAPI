51499be7dbf5f78ffecae34e7c9bc19261539b19
hungsvkt95-bit <hungsvkt95@gmail.com>
2026-05-19 18:15:52 +0700

    Bai2 HelloWorld

---
+++ L2HelloWorld/HelloWorldCmd.cs
@@ -0,0 +1,25 @@
+﻿using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace L2HelloWorld
+{
+    [Transaction(TransactionMode.Manual)]
+    public class HelloWorldCmd : IExternalCommand
+    {
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            
+
+            MessageBox.Show(messageBoxText:"Hello World", caption:"Revit API 2024", MessageBoxButton.OK, MessageBoxImage.Information);
+
+            return Result.Succeeded;
+        }
+    }
+}
