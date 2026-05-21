eb1510083342dd37854375df148fbf604dbd7616
hungsvkt95-bit <hungsvkt95@gmail.com>
2026-05-21 20:56:26 +0700

    RevitAIApp_BaiTapB1

---
+++ RevitAIApp/RevitAIAppAplication/Application.cs
@@ -0,0 +1,50 @@
+﻿using Nice3point.Revit.Toolkit.External;
+using RevitAIAppAplication.Commands;
+using Serilog;
+using Serilog.Events;
+
+namespace RevitAIAppAplication
+{
+    /// <summary>
+    ///     Application entry point
+    /// </summary>
+    [UsedImplicitly]
+    public class Application : ExternalApplication
+    {
+        public override void OnStartup()
+        {
+            CreateLogger();
+            CreateRibbon();
+        }
+
+        public override void OnShutdown()
+        {
+            Log.CloseAndFlush();
+        }
+
+        private void CreateRibbon()
+        {
+            var panel = Application.CreatePanel("Commands", "RevitAIAppAplication");
+
+            panel.AddPushButton<StartupCommand>("Execute")
+                .SetImage("/RevitAIAppAplication;component/Resources/Icons/RibbonIcon16.png")
+                .SetLargeImage("/RevitAIAppAplication;component/Resources/Icons/RibbonIcon32.png");
+        }
+
+        private static void CreateLogger()
+        {
+            const string outputTemplate = "{Timestamp:yyyy-MM-dd HH:mm:ss} [{Level:u3}] {Message:lj}{NewLine}{Exception}";
+
+            Log.Logger = new LoggerConfiguration()
+                .WriteTo.Debug(LogEventLevel.Debug, outputTemplate)
+                .MinimumLevel.Debug()
+                .CreateLogger();
+
+            AppDomain.CurrentDomain.UnhandledException += (_, args) =>
+            {
+                var exception = (Exception)args.ExceptionObject;
+                Log.Fatal(exception, "Domain unhandled exception");
+            };
+        }
+    }
+}
